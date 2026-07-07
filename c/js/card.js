1431c14fff7cc20246f5f866c032a84fc079a998
Prawez <praweza421@gmail.com>
2026-07-07 17:11:12 +0530

    Your commit message c folder mai

--- c/js/card.js
+++ c/js/card.js
@@ -5,9 +5,13 @@ function createCodeCard({
   title,
   code,
 }) {
+
   const container = document.getElementById(containerId);
 
-  if (!container) return;
+  if (!container) {
+    console.log("Container not found");
+    return;
+  }
 
   container.innerHTML += `
     <div class="bg-gradient-to-r from-[#121826] to-[#1b2440] rounded-[30px] border border-indigo-500 overflow-hidden shadow-xl">
@@ -20,10 +24,25 @@ function createCodeCard({
 
       <div class="p-6">
         <div class="bg-[#0b1120] border-l-[6px] border-blue-500 rounded-xl p-8 overflow-x-auto">
+
           <pre class="text-lg md:text-xl leading-8 text-white whitespace-pre-wrap"><code>${code}</code></pre>
+
         </div>
       </div>
 
     </div>
   `;
 }
+
+
+createCodeCard({
+  containerId: "codeCards",
+  title: "Hello World Example",
+  code: `#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+    return 0;
+}`
+});
--- c/js/sidebar.js
+++ c/js/sidebar.js
@@ -1,8 +1,48 @@
 function createSidebar(activeIndex = 0) {
+
+  // CSS Inject
+  if (!document.getElementById("sidebar-style")) {
+    const style = document.createElement("style");
+    style.id = "sidebar-style";
+    style.innerHTML = `
+      .nav-link-active::after{
+        content:"";
+        position:absolute;
+        left:0;
+        bottom:-8px;
+        width:100%;
+        height:2px;
+        background:#4d6fff;
+        border-radius:10px;
+      }
+
+      .custom-scrollbar::-webkit-scrollbar{
+         width:3px;     
+         height:3px; 
+      }
+
+      .custom-scrollbar::-webkit-scrollbar-track{
+        background:#020617;
+      }
+
+      .custom-scrollbar::-webkit-scrollbar-thumb{
+        background:#264dff;
+        border-radius:10px;
+      }
+
+      .custom-scrollbar{
+        scrollbar-width:thin;
+        scrollbar-color:#264dff #020617;
+      }
+    `;
+
+    document.head.appendChild(style);
+  }
+
   const menus = [
     { name: "C HOME", link: "home.html" },
     { name: "C Intro", link: "intro.html" },
-    { name: "C Get Started", link: "get-started.html" },
+    { name: "C Get Started", link: "getstarted.html" },
     { name: "C Syntax", link: "syntax.html" },
     { name: "C Output", link: "output.html" },
     { name: "C Comments", link: "comments.html" },
@@ -31,11 +71,9 @@ function createSidebar(activeIndex = 0) {
     <aside
       class="hidden md:block sticky top-[110px] left-0 w-[340px]
       h-[calc(100vh-110px)] overflow-y-auto custom-scrollbar
-      bg-[#020b24] p-5 border-r border-[#264dff] z-[50]"
+      bg-[#020b24] p-5  z-[50]"
     >
-      <h5
-        class="text-[#7f8db4] tracking-[5px] text-[20px] font-bold mb-5 uppercase"
-      >
+      <h5 class="text-[#7f8db4] tracking-[5px] text-[20px] font-bold mb-5 uppercase">
         C Tutorial
       </h5>
 
