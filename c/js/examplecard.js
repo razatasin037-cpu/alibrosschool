9c889fb50a2b81ce39e3c7d20221ae7da28ee62b
Prawez <praweza421@gmail.com>
2026-07-11 17:10:57 +0530

    c change

--- c/js/examplecard.js
+++ c/js/examplecard.js
@@ -17,15 +17,15 @@ function createCodeCard({
     <div class="bg-gradient-to-r from-[#121826] to-[#1b2440] rounded-[30px] border border-indigo-500 overflow-hidden shadow-xl">
 
       <div class="p-8 border-b border-white/10">
-        <h2 class="text-3xl md:text-4xl font-bold text-white">
+        <h2 class="text-2xl md:text-4xl font-bold text-white">
           ${title}
         </h2>
       </div>
 
       <div class="p-6">
-        <div class="bg-[#0b1120] border-l-[6px] border-blue-500 rounded-xl p-8 overflow-x-auto">
+        <div class="bg-[#0b1120] border-l-[6px] border-blue-500 rounded-xl p-4 md:p-8 overflow-x-auto">
 
-          <pre class="text-lg md:text-xl leading-8 text-white whitespace-pre-wrap"><code>${code}</code></pre>
+          <pre class="text-md md:text-xl leading-8 text-white whitespace-pre-wrap"><code>${code}</code></pre>
 
         </div>
       </div>
@@ -38,11 +38,11 @@ function createCodeCard({
 createCodeCard({
   containerId: "codeCards",
   title: "Hello World Example",
-  code: `#include <stdio.h>
+  code: `#include &lt;stdio.h&gt;
 
-int main()
+<span class="text-green-400">int</span> main()
 {
-    printf("Hello World");
-    return 0;
+    printf(<span class="text-green-400">"Hello World"</span>);
+    <span class="text-green-400">return 0</span>;
 }`
 });
--- c/js/sidebar.js
+++ c/js/sidebar.js
@@ -5,20 +5,8 @@ function createSidebar(activeIndex = 0) {
     const style = document.createElement("style");
     style.id = "sidebar-style";
     style.innerHTML = `
-      .nav-link-active::after{
-        content:"";
-        position:absolute;
-        left:0;
-        bottom:-8px;
-        width:100%;
-        height:2px;
-        background:#4d6fff;
-        border-radius:10px;
-      }
-
       .custom-scrollbar::-webkit-scrollbar{
-         width:3px;     
-         height:3px; 
+        width:3px;
       }
 
       .custom-scrollbar::-webkit-scrollbar-track{
@@ -35,7 +23,6 @@ function createSidebar(activeIndex = 0) {
         scrollbar-color:#264dff #020617;
       }
     `;
-
     document.head.appendChild(style);
   }
 
@@ -43,43 +30,106 @@ function createSidebar(activeIndex = 0) {
     { name: "C HOME", link: "home.html" },
     { name: "C Intro", link: "intro.html" },
     { name: "C Get Started", link: "getstarted.html" },
-    { name: "C Syntax", link: "syntax.html" },
+
+    {
+      name: "C Syntax" ,
+      submenu: [
+        // { name: "Syntax", link: "Syntax.html" },
+        { name: "Statements", link: "statements.html" },
+        { name: "Syntax Code Challenge", link: "syntax-challenge.html" }
+      ]
+    },
+
     { name: "C Output", link: "output.html" },
     { name: "C Comments", link: "comments.html" },
     { name: "C Variables", link: "variables.html" },
     { name: "C User Input", link: "user-input.html" },
     { name: "C Data Types", link: "data-types.html" },
-    { name: "C Operators", link: "operators.html" },
+    { name: "C Operators", link: "operators.html" }
   ];
 
   let menuHTML = "";
 
   menus.forEach((menu, index) => {
-    menuHTML += `
-      <a
-        href="${menu.link}"
-        class="block py-[18px] px-[35px] text-white text-[18px] font-medium rounded-[18px]
-        ${index === activeIndex ? "bg-[#3d5dff]" : "hover:bg-[#3d5dff]"}
-        transition duration-300"
-      >
-        ${menu.name}
-      </a>
-    `;
+
+// syntax
+    if (menu.submenu) {
+
+      menuHTML += `
+        <div>
+
+          <button
+            class="submenu-btn  w-full flex justify-between items-center py-[18px] px-[35px]
+            text-white text-[18px] font-bold rounded-[18px] hover:bg-[#3d5dff] transition"
+          >
+            <span>${menu.name}</span>
+            <i class="text-xl text-[#9c9a9a] fa-solid fa-caret-right"></i>
+          </button>
+
+          <div class="submenu hidden ml-8 mt-2 flex flex-col gap-2">
+      `;
+// syntax drop down
+      menu.submenu.forEach(item => {
+        menuHTML += `
+          <a
+            href="${item.link}"
+            class="block px-4 py-2 rounded-lg font-bold text-gray-300 hover:bg-[#3158ff] hover:text-white transition"
+          >
+             ${item.name}
+          </a>
+        `;
+      });
+
+      menuHTML += `
+          </div>
+
+        </div>
+      `;
+
+    } else {
+   // sidebar link a tag sab
+      menuHTML += `
+        <a
+          href="${menu.link}"
+          class="block py-[18px] px-[35px] text-white text-[18px] font-bold rounded-[18px]
+          ${index === activeIndex ? "bg-[#3d5dff]" : "hover:bg-[#3d5dff]"}
+          transition duration-300"
+        >
+          ${menu.name}
+        </a>
+      `;
+
+    }
+
   });
-
+// sidebar 
   return `
-    <aside
-      class="hidden md:block sticky top-[110px] left-0 w-[340px]
-      h-[calc(100vh-110px)] overflow-y-auto custom-scrollbar
-      bg-[#020b24] p-5  z-[50]"
-    >
-      <h5 class="text-[#7f8db4] tracking-[5px] text-[20px] font-bold mb-5 uppercase">
-        C Tutorial
-      </h5>
-
-      <nav class="flex flex-col gap-3">
-        ${menuHTML}
-      </nav>
-    </aside>
+    <aside class="hidden md:block sticky top-[110px] left-0 w-[300px] h-[calc(100vh-110px)] overflow-y-auto custom-scrollbar bg-[#020b24] p-5 z-[50]" >
+     <h5 class="text-[#7f8db4] tracking-[5px] ml-10 text-[20px] font-bold mb-5 uppercase"> C Tutorial </h5>
+      <nav class="flex flex-col gap-3 "> ${menuHTML} </nav>
+       </aside>
   `;
 }
+
+// Dropdown Function
+document.addEventListener("click", function (e) {
+
+  const btn = e.target.closest(".submenu-btn");
+  if (!btn) return;
+
+  const submenu = btn.nextElementSibling;
+  const icon = btn.querySelector("i");
+
+  submenu.classList.toggle("hidden");
+
+  icon.classList.toggle("fa-caret-right");
+  icon.classList.toggle("fa-caret-down");
+
+  // Background active
+  if (submenu.classList.contains("hidden")) {
+    btn.style.backgroundColor = "";
+  } else {
+    btn.style.backgroundColor = "#3d5dff";
+  }
+
+});
